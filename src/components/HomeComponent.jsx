36a9db6f91b733a4683aec7720a9d99a7277cf0e
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-11-27 20:11:58 -0300

    fix: adjust font sizes in HomeComponent for improved readability

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -3,18 +3,18 @@ import React from "react";
 export default function HomeComponent() {
   return (
     <div className="flex flex-col sm:mx-[40px] md:mx-[200px]  md:w-[35vw] sm:justify-center items-center ">
-      <h1 className="pt-4 lg:text-[3.2rem] md:text-[2.2rem] text-[2.2rem] text-medium  sm:justify-center text-center md:text-left sm:items-center text-black dark:text-white  ">
+      <h1 className="pt-4 lg:text-[2.2rem] md:text-[2.2rem] text-[1.8rem] text-medium  sm:justify-center text-center md:text-left sm:items-center text-black dark:text-white  ">
         {" "}
         Ajudando empresas a construirem <br />
         <span className="text-[#986dff]"> produtos digitais </span> de forma
         moderna e de alta qualidade
       </h1>
-      <p className="text-gray-500 text-medium text-[2rem] text-center md:text-left sm:items-center">
+      <p className="text-gray-500 text-medium text-[1.5rem] text-center md:text-left sm:items-center">
         Design e desenvolvimento profissional que vão destacar a sua loja no
         mercado.
       </p>
       {/* FORM PARA ENCAMINHAR PARA O MEU EMAIL informações necessarias: e-mail da pessoa, nome, número de telefone e informações sobre a empresa */}
-      <button className="bg-[#986dff] text-white text-[1.5rem] font-semibold rounded-sm w-[300px] h-12 my-3">
+      <button className="bg-[#986dff] text-white text-[1.2rem] font-semibold rounded-sm w-[300px] h-12 my-3">
         Solicitar serviço
       </button>
     </div>
