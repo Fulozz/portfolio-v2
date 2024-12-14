1c16fdef55934be2f00dbcbce754a12e4db25719
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-12-14 18:37:12 -0300

    refactor: restructure NavbarComponent for improved layout and navigation functionality

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -4,10 +4,14 @@ import ThemeToggle from './theme/ThemeToggle'
 import { Menu, X } from 'lucide-react';
 import DownloadButton from './DownloadButton';
 import Image from 'next/image';
+import Link from 'next/link';
+import logo from "./assets/logo.png"
 
 export default function NavbarComponent() {
     const [isOpen, setIsOpen] = useState(false);
-    const toggleMenu = () => setIsOpen(!isOpen);
+    const toggle = () => {
+      setIsOpen(!isOpen);
+    };
     const navItems = [
         {
             name: 'Home',
@@ -27,41 +31,58 @@ export default function NavbarComponent() {
         }
     ]
   return (
-    <div className="flex items-center justify-between  text-black dark:text-white h-20 py-2 px-8 mx-[60px] my-4 ">
-        <div className='flex'>
-            <a href="#" className='font-semibold w-20'> <Image src='/logo.png' height={40} width={40} alt="logo"/></a>
-        </div>
-        <div className=" items-center hidden md:flex lg:flex">
-        <DownloadButton href={"http://fulozz.vercel.app/assets/Thiago Silva Andrade - Desenvolvimento de Software.pdf"} filename={"Thiago Silva Andrade - Desenvolvimento de Software.pdf"} />
-            {navItems.map((item, index) => {
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              {/* Seu logo ou título aqui */}
+              <Image src={logo} alt="Logo" width={60} height={60} />	
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+              {
+              navItems.map((item, index) => {
                 return (
-                    <a key={index} href={item.href} className='mx-4 font-normal text-black dark:text-white hover:text-slate-300' download={item.download}>{item.name}</a>
-                )
-            })}
-            <ThemeToggle />
-        </div>
-        <div className={`fixed top-0 left-0 w-full h-[90px] md:hidden bg-gray-900/90 z-50 transition duration-300 ease-in-out ${isOpen ? 'block' : 'hidden'}`}>
-        
-          <div className="flex flex-col items-center justify-center text-center h-full">
-            <ul className="space-y-4 items-center">
-              {navItems.map((item, index) => (
-                <li key={index} className='pb-4'>
-                  <a
-                    href={item.href}
-                    className="text-white hover:bg-gray-700 hover:text-purple-500 px-3 py-2 rounded-md text-xl font-medium mt-4"
-                    onClick={toggleMenu}
-                  >
-                    {item.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-            <ThemeToggle />
+                    <Link key={index} href={item.href} className='mx-4 font-normal text-black dark:text-white hover:text-slate-300' download={item.download}>{item.name}</Link>
+                  )
+                })
+              }
+                {/* Outros links aqui */}
+              </div>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggle}
+              type="button"
+              className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {/* Hamburger icon */}
+              <Menu />
+            </button>
           </div>
         </div>
-        <div className="md:hidden lg:hidden z-50 justify-center" onClick={toggleMenu}>
-           {isOpen ? (<X className='fixed'/>) :( <Menu className='fixed'/>)}
+      </div>
+
+      <div className={`md:hidden ${isOpen ? 'block' : 'hidden'} flex flex-col space-y-4`}>
+        <div className=" px-2 pt-2 pb-4 sm:px-3 justify-center text-center">
+          
+          {
+            navItems.map((item, index)=> {
+              return(
+                <Link href="/" key={index} className="block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-[1.5rem] font-medium">
+                  {item.name}
+              </Link>
+              )
+            })
+          }
+          {/* Outros links aqui */}
         </div>
-    </div>
+      </div>
+    </nav>
   )
 }
